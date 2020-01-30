2ade16e88329addbfaa98705c2b1de2a07099110
Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>
2020-01-30 12:28:43 +0000

    Added storageclasses with statefulsets

--- eks/cluster.tf
+++ eks/cluster.tf
@@ -68,6 +68,7 @@ module "vpc" {
 
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
+  version      = "8.1.0"
   cluster_name = local.cluster_name
   subnets      = module.vpc.public_subnets
   vpc_id       = module.vpc.vpc_id
