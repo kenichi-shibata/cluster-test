f9dc2a64526be453e0aeb8414771a9fd9b496ee9
Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>
2020-07-01 15:23:33 +0100

    Datadog Collect k8s Events

--- observability/datadog/deploy_datadog.sh
+++ observability/datadog/deploy_datadog.sh
@@ -8,6 +8,11 @@ helm3 upgrade --install -f datadog-agent.yaml \
 --namespace=monitoring \
 --set datadog.processAgent.enabled=true \
 --set datadog.processAgent.processCollection=true \
+--set datadog.leaderElection=true \
+--set datadog.collectEvents=true \
+--set agents.rbac.create=true \
 --set datadog.site='datadoghq.eu' \
 --set datadog.apiKey=${DATADOG_API_KEY} \
 datadog-agent stable/datadog
+
+# https://docs.datadoghq.com/agent/kubernetes/?tab=helm
